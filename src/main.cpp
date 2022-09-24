aebeef2e5c730fa25413398046128def8677c6c5
R2leyser <51343917+R2leyser@users.noreply.github.com>
2022-09-24 16:12:42 -0300

    Added file reading

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main(int argc, char *argv[])
 {
+        std::string line;
+        std::ifstream milfCode("Code.milf"); //open file
+        //read line by libe 
+        while (getline (milfCode, line)) {
+                std::cout << line << std::endl;
+        }
+        milfCode.close(); //close file
+
         std::cout << "This is going to be a compiler at some point" << std::endl;
         return 0;
 }
