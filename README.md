4ddb100dafa3aa2432f83649e4a0ace680b3bb48
R2leyser <51343917+R2leyser@users.noreply.github.com>
2022-09-25 19:26:00 -0300

    Added some token handling stuff, updated readme + other random shit

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 this is a project that I'm doing just for fun
 
 if you use this in production for whatever reason.... what is wrong with you
+
+## About what I'm doing 
+
+- This is going to compile to LLVM intermediate representation
+- If I make a good enough language I'm gonna rewrite the compiler in it so that is going to be fun
+
+I'm doing it in C++ because I felt like coding in it, I'm far from good at writing C++ code
--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,70 @@
 #include <fstream>
 #include <string>
 
+void F_PRINT(std::string string){
+
+        std::string::size_type i = string.find("print");
+        if (i != std::string::npos)
+                string.erase(i, 5);
+
+        std::cout << string << std::endl; 
+}
+
+/*! \enum tokens
+ *
+ *  list of valid supported tokens
+ */
+enum tokens { 
+        PRINT = 1,
+        HI = 2,
+};
+
+tokens getToken (std::string const& inString) {
+    if (inString == "print") return PRINT;
+    if (inString == "hi") return HI;
+}
+
+int count{};
+
+auto iota(){
+        count++;
+}
+
 int main(int argc, char *argv[]) {
+        
+        if (argc < 2) {
+                fprintf(stderr, "Please provide a path\n" );
+                return -1;
+        }
+
         std::string line;
-        std::ifstream milfCode(argv[argc-1]); //open file
+        char *filePath{argv[argc-1]};
+        std::ifstream milfCode(filePath); //open file
+        std::string kw;
 
         if (!milfCode) {
-                std::cerr << "File not found" << std::endl;
+                fprintf(stderr, "File path %s is not valid\n", filePath);
+                return -1;
         }
 
+        tokens TOKEN;
         //read line by line 
-        while (getline (milfCode, line)) {
-                std::cout << line << std::endl;
-        }
+        while (getline (milfCode, line, ';')) {
+                TOKEN = getToken(line);
+        //TODO: Figure out why this doesn't detect the HI token
 
+                printf("Line: %s\n", line);
+                if (PRINT){
+                        F_PRINT(line);
+                        break;
+                }
+                else if (HI){
+                        printf("Hello!\n");
+                }
+                iota();
+        };
+        iota();
+        printf("%s\n", line);
         return 0;
 }
+//vim:filetype=cpp
